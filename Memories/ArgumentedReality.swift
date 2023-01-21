0ee02667017a86d4915f212594bd2583c7adb569
Caio Stoduto <88816833+caiostoduto@users.noreply.github.com>
2023-01-20 22:27:35 -0300

    🧹 chore: removing unnecessary code

--- Memories/ArgumentedReality.swift
+++ Memories/ArgumentedReality.swift
@@ -60,34 +60,5 @@ private extension CGImage {
         context.draw(self, in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
         
         return context.makeImage()
-        
-        /* let width: Int = Int(size.width)
-        let height: Int = Int(size.height)
-
-        let bytesPerPixel = self.bitsPerPixel / self.bitsPerComponent
-        let destBytesPerRow = width * bytesPerPixel
-
-
-        guard let colorSpace = self.colorSpace else { return nil }
-        guard let context = CGContext(data: nil, width: width, height: height, bitsPerComponent: self.bitsPerComponent, bytesPerRow: destBytesPerRow, space: colorSpace, bitmapInfo: self.alphaInfo.rawValue) else { return nil }
-
-        context.interpolationQuality = .high
-        context.draw(self, in: CGRect(x: 0, y: 0, width: width, height: height))
-
-        return context.makeImage() */
-    }
-}
-
-private extension UIImage {
-    func scalePreservingAspectRatio(targetSize: CGSize) -> UIImage {
-        let widthRatio = targetSize.width / size.width
-        let heightRatio = targetSize.height / size.height
-        let scaleFactor = min(widthRatio, heightRatio)
-        let scaledImageSize = CGSize(width: size.width * scaleFactor, height: size.height * scaleFactor)
-
-        let renderer = UIGraphicsImageRenderer(size: scaledImageSize)
-        return renderer.image { _ in
-            draw(in: CGRect(origin: .zero, size: scaledImageSize))
-        }
     }
 }
---
+++ Memories/Views/Create/SelectVideoView.swift
@@ -0,0 +1,20 @@
+//
+//  SelectVideoView.swift
+//  Memories
+//
+//  Created by Caio Stoduto on 20/01/23.
+//
+
+import SwiftUI
+
+struct SelectVideoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectVideoView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectVideoView()
+    }
+}
